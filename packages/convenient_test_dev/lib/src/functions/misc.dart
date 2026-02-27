5825025a08e03c97b3785bd1cf986bf589542c2c
Igor Shahin <igor.shahin@icloud.com>
2026-02-28 01:24:33 +0300

    make restartApp robust with runAsyncEnhanced and ui reset

--- packages/convenient_test_dev/lib/src/functions/misc.dart
+++ packages/convenient_test_dev/lib/src/functions/misc.dart
@@ -1,6 +1,7 @@
 import 'package:convenient_test_dev/convenient_test_dev.dart';
 import 'package:convenient_test_dev/src/support/get_it.dart';
 import 'package:convenient_test_dev/src/support/static_config.dart';
+import 'package:flutter/widgets.dart';
 
 extension ConvenientTestMisc on ConvenientTest {
   String get appCodeDir => StaticConfig.kAppCodeDir;
@@ -11,9 +12,14 @@ extension ConvenientTestMisc on ConvenientTest {
   Future<void> restartApp({
     bool settle = true,
     String stepTitle = 'APP RESTART',
+    bool hardResetUiTree = true,
   }) async {
     convenientTestLog(stepTitle, '');
-    await tester.runAsync(() async {
+    if (hardResetUiTree) {
+      await tester.pumpWidget(const SizedBox.shrink());
+      await tester.pumpWithRunAsync();
+    }
+    await tester.runAsyncEnhanced(() async {
       await myGetIt.get<ConvenientTestSlot>().appMain(
             AppMainExecuteMode.integrationTest,
           );
